68aca1aa6a89aedbe9ef69da9a29a16293a69e0b
ashupal-career <ashuvatsal007@gmail.com>
2026-04-05 16:07:02 +0530

    Create a paste.jsx

--- paste-app/src/components/Paste.jsx
+++ paste-app/src/components/Paste.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeFromPastes } from '../redux/pasteSlice';
+import toast from 'react-hot-toast';
 
 const Paste = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const pastes = useSelector((state) => state.paste.pastes);
+
+  const dispatch = useDispatch();
+
+  const filteredData = pastes.filter((paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  function handleDelete(pasteId) {
+    dispatch(removeFromPastes(pasteId));
+  }
+
   return (
     <div>
-        List of Pastes
+        <input 
+          className='p-2 rounded-2xl min-w-[600px] mt-5'
+          type="search" 
+          placeholder='search here'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <div className='flex flex-col gap-5 mt-5'>
+          {
+            filteredData.length > 0 &&
+            filteredData.map(
+              (paste) => {
+                return (
+                  <div className='border' key={paste?._id}>
+                    <div>
+                      {paste.title}
+                    </div>
+                    <div>
+                      {paste.content}
+                    </div>
+                    <div className='flex flex-row gap-4 palce-content-evenly'>
+                      <button>Edit</button>
+                      <button>View</button>
+                      <button onClick={() => handleDelete(paste?._id)}>Delete</button>
+                      <button onClick={() => {navigator.clipboard.writeText(paste?.content), toast.success("copied to clipboard")}}>Copy</button>
+                      <button>Share</button>
+                    </div>
+                    <div>
+                      {paste.createdAt}
+                    </div>
+                  </div>
+                )
+              }
+            )
+          }
+        </div>
     </div>
   )
 }
